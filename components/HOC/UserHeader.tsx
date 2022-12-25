b729cb6d2cebc7e310b47513336303166e66ea6e
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-26 00:55:49 +0600

    Refactoring redux and ant.

--- components/HOC/UserHeader.tsx
+++ components/HOC/UserHeader.tsx
@@ -1,22 +1,28 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { RootState } from '../../store/configureStore'
+import { connect, useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../store/configureStore'
 import { fetchUser } from '../../store/users'
 
 const UserHeader = (props: any) => {
+  const dispatch = useDispatch<AppDispatch>()
+  const user = useSelector((state: RootState) =>
+    state.entities.users.list.find((user) => user.id === props.userId)
+  )
+
   useEffect(() => {
-    props.fetchUser(props.userId)
+    dispatch(fetchUser(props.userId))
   }, [])
 
-  if (!props.user) return <div>NO User found!</div>
+  if (!user) return <div>NO User found!</div>
 
-  return <div className="header" style={{ marginBottom: '0', marginLeft: '10px', marginRight: '10px'}}>{props.user.name}</div>
+  return (
+    <div
+      className="header"
+      style={{ marginBottom: '0', marginLeft: '10px', marginRight: '10px' }}
+    >
+      {user.name}
+    </div>
+  )
 }
 
-const mapStateToProps = (state: RootState, ownProps: any) => {
-  return {
-    user: state.entities.users.list.find((user) => user.id === ownProps.userId),
-  }
-}
-
-export default connect(mapStateToProps, { fetchUser })(UserHeader)
+export default UserHeader
